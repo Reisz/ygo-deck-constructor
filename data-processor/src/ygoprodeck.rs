d22f685f39975eea8167a8e1896c44558da08491
Reisz <Reisz@users.noreply.github.com>
2023-02-21 22:43:39 +0100

    Add definitions for more YGOProDeck API fields

--- data-processor/src/ygoprodeck.rs
+++ data-processor/src/ygoprodeck.rs
@@ -1,10 +1,12 @@
+//! YGOPRODeck API v7 wrapper (See [documentation](https://ygoprodeck.com/api-guide/)).
+
 use std::io::Read;
 
 use anyhow::Result;
 use serde::Deserialize;
 
 pub const VERSION_URL: &str = "https://db.ygoprodeck.com/api/v7/checkDBVer.php";
-pub const URL: &str = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+pub const URL: &str = "https://db.ygoprodeck.com/api/v7/cardinfo.php?format=tcg";
 
 #[derive(Debug, Deserialize)]
 struct VersionInfo {
@@ -13,9 +15,143 @@ struct VersionInfo {
 
 #[derive(Debug, Deserialize)]
 pub struct Card {
+    // All Cards
     pub id: u64,
     pub name: String,
+    #[serde(rename = "type")]
+    pub card_type: CardType,
+    // Unused: frameType
     pub desc: String,
+
+    // Monster Cards
+    pub atk: Option<u16>,
+    pub def: Option<u16>,
+    pub level: Option<u8>,
+    // NOTE: Not using enum for race due to irregularities
+    pub race: Option<String>,
+    pub attribute: Option<Attribute>,
+
+    // Spell/Trap Cards
+    // Duplicate: race
+
+    // Card Archetype
+    pub archetype: Option<String>,
+
+    // Additional Response for Pendulum Monsters
+    pub scale: Option<u8>,
+
+    // Additional Response for Link Monsters
+    pub linkval: Option<u8>,
+    pub linkmarkers: Option<Vec<LinkMarker>>,
+
+    pub banlist_info: Option<BanlistInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum CardType {
+    // Main Deck Types
+    #[serde(rename = "Effect Monster")]
+    EffectMonster,
+    #[serde(rename = "Flip Effect Monster")]
+    FlipEffectMonster,
+    #[serde(rename = "Flip Tuner Effect Monster")]
+    FlipTunerEffectMonster,
+    #[serde(rename = "Gemini Monster")]
+    GeminiMonster,
+    #[serde(rename = "Normal Monster")]
+    NormalMonster,
+    #[serde(rename = "Normal Tuner Monster")]
+    NormalTunerMonster,
+    #[serde(rename = "Pendulum Effect Monster")]
+    PendulumEffectMonster,
+    #[serde(rename = "Pendulum Effect Ritual Monster")]
+    PendulumEffectRitualMonster,
+    #[serde(rename = "Pendulum Flip Effect Monster")]
+    PendulumFlipEffectMonster,
+    #[serde(rename = "Pendulum Normal Monster")]
+    PendulumNormalMonster,
+    #[serde(rename = "Pendulum Tuner Effect Monster")]
+    PendulumTunerEffectMonster,
+    #[serde(rename = "Ritual Effect Monster")]
+    RitualEffectMonster,
+    #[serde(rename = "Ritual Monster")]
+    RitualMonster,
+    #[serde(rename = "Spell Card")]
+    SpellCard,
+    #[serde(rename = "Spirit Monster")]
+    SpiritMonster,
+    #[serde(rename = "Toon Monster")]
+    ToonMonster,
+    #[serde(rename = "Trap Card")]
+    TrapCard,
+    #[serde(rename = "Tuner Monster")]
+    TunerMonster,
+    #[serde(rename = "Union Effect Monster")]
+    UnionEffectMonster,
+
+    // Extra Deck Types
+    #[serde(rename = "Fusion Monster")]
+    FusionMonster,
+    #[serde(rename = "Link Monster")]
+    LinkMonster,
+    #[serde(rename = "Pendulum Effect Fusion Monster")]
+    PendulumEffectFusionMonster,
+    #[serde(rename = "Synchro Monster")]
+    SynchroMonster,
+    #[serde(rename = "Synchro Pendulum Effect Monster")]
+    SynchroPendulumEffectMonster,
+    #[serde(rename = "Synchro Tuner Monster")]
+    SynchroTunerMonster,
+    #[serde(rename = "XYZ Monster")]
+    XYZMonster,
+    #[serde(rename = "XYZ Pendulum Effect Monster")]
+    XYZPendulumEffectMonster,
+
+    // Other Types
+    #[serde(rename = "Skill Card")]
+    SkillCard,
+    Token,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Attribute {
+    Dark,
+    Earth,
+    Fire,
+    Light,
+    Water,
+    Wind,
+    Divine,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum LinkMarker {
+    Top,
+    Bottom,
+    Left,
+    Right,
+    #[serde(rename = "Bottom-Left")]
+    BottomLeft,
+    #[serde(rename = "Bottom-Right")]
+    BottomRight,
+    #[serde(rename = "Top-Left")]
+    TopLeft,
+    #[serde(rename = "Top-Right")]
+    TopRight,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BanlistInfo {
+    pub ban_tcg: Option<BanStatus>,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum BanStatus {
+    Limited,
+    #[serde(rename = "Semi-Limited")]
+    SemiLimited,
+    Banned,
 }
 
 #[derive(Debug, Deserialize)]
