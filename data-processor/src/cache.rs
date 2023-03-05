1f5ca91552d83d6410c5fdf0e54b958a70238f8b
Reisz <Reisz@users.noreply.github.com>
2023-03-05 21:24:47 +0100

    Improve cache behavior

---
+++ data-processor/src/cache.rs
@@ -0,0 +1,69 @@
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader},
+    path::Path,
+    time::{Duration, SystemTime},
+};
+
+use anyhow::Result;
+
+use crate::{step, ygoprodeck, CARD_INFO_LOCAL, OUTPUT_FILE};
+
+const VERSION_CHECK_INTERVAL: Duration = Duration::from_secs(60 * 60 * 24); // 1 day
+
+#[derive(Debug, Clone)]
+pub enum CacheBehavior {
+    Download { online_version: String },
+    Process,
+    Nothing,
+}
+
+pub fn get_behavior() -> Result<CacheBehavior> {
+    Ok(if let Some(online_version) = should_download()? {
+        CacheBehavior::Download { online_version }
+    } else if should_process()? {
+        CacheBehavior::Process
+    } else {
+        CacheBehavior::Nothing
+    })
+}
+
+fn should_download() -> Result<Option<String>> {
+    step("Checking online database version");
+
+    let online_version = ygoprodeck::get_version(reqwest::blocking::get(ygoprodeck::VERSION_URL)?)?;
+
+    if !Path::new(CARD_INFO_LOCAL).exists() {
+        return Ok(Some(online_version));
+    }
+
+    let cache_date = fs::metadata(CARD_INFO_LOCAL)?.modified()?;
+    if cache_date.elapsed()? > VERSION_CHECK_INTERVAL {
+        // Reset interval
+        File::open(CARD_INFO_LOCAL)?.set_modified(SystemTime::now())?;
+
+        let local_version = {
+            let mut tmp = String::new();
+            BufReader::new(File::open(CARD_INFO_LOCAL)?).read_line(&mut tmp)?;
+            tmp.truncate(tmp.len() - 1); // Remove trailing newline
+            tmp
+        };
+
+        if local_version != online_version {
+            return Ok(Some(online_version));
+        }
+    }
+
+    Ok(None)
+}
+
+fn should_process() -> Result<bool> {
+    let output_date = fs::metadata(OUTPUT_FILE)?.modified()?;
+    let executable_date = fs::metadata(std::env::current_exe()?)?.modified()?;
+
+    if executable_date > output_date {
+        return Ok(true);
+    }
+
+    Ok(false)
+}
--- data-processor/src/main.rs
+++ data-processor/src/main.rs
@@ -1,12 +1,13 @@
+#![feature(file_set_times)]
+
 use std::{
     fs::{self, File},
     io::{self, BufRead, BufReader, BufWriter, Read, Write},
     os::unix::prelude::MetadataExt,
-    path::Path,
 };
 
 use anyhow::{anyhow, Result};
-use clap::Parser;
+use cache::CacheBehavior;
 use data::card::CardData;
 use indicatif::{
     DecimalBytes, HumanCount, ParallelProgressIterator, ProgressIterator, ProgressStyle,
@@ -15,9 +16,9 @@ use rayon::prelude::*;
 use serde::{ser::SerializeMap, Serializer};
 use xz2::write::XzEncoder;
 
-use crate::{cli::Args, reqwest_indicatif::ProgressReader};
+use crate::reqwest_indicatif::ProgressReader;
 
-mod cli;
+mod cache;
 mod project;
 mod reqwest_indicatif;
 mod ygoprodeck;
@@ -42,32 +43,8 @@ fn get_card_info_download() -> Result<impl Read> {
     Ok(BufReader::new(ProgressReader::from_response(response)))
 }
 
-fn get_card_info(cached: bool) -> Result<Vec<ygoprodeck::Card>> {
-    if !cached {
-        return ygoprodeck::parse(get_card_info_download()?);
-    }
-
-    step("Checking online database version");
-    let online_version = ygoprodeck::get_version(reqwest::blocking::get(ygoprodeck::VERSION_URL)?)?;
-
-    let mut cache_ok = Path::new(CARD_INFO_LOCAL).exists();
-    if cache_ok {
-        let local_version = {
-            let mut tmp = String::new();
-            BufReader::new(File::open(CARD_INFO_LOCAL)?).read_line(&mut tmp)?;
-            tmp.truncate(tmp.len() - 1); // Remove trailing newline
-            tmp
-        };
-
-        if local_version == online_version {
-            println!("  Version: {local_version}");
-        } else {
-            println!("  Version: {local_version} (out of date, current: {online_version})");
-            cache_ok = false;
-        }
-    }
-
-    if !cache_ok {
+fn get_card_info(cache_behavior: CacheBehavior) -> Result<Vec<ygoprodeck::Card>> {
+    if let CacheBehavior::Download { online_version } = cache_behavior {
         let mut local_file = BufWriter::new(File::create(CARD_INFO_LOCAL)?);
         writeln!(local_file, "{online_version}")?;
         io::copy(&mut get_card_info_download()?, &mut local_file)?;
@@ -80,12 +57,16 @@ fn get_card_info(cached: bool) -> Result<Vec<ygoprodeck::Card>> {
 }
 
 fn main() -> Result<()> {
-    let args = Args::try_parse()?;
-
     let style =
         ProgressStyle::with_template("{bar} {human_pos}/{human_len} ({eta} remaining)").unwrap();
 
-    let cards = get_card_info(args.cache)?;
+    let cache = cache::get_behavior()?;
+    if matches!(cache, CacheBehavior::Nothing) {
+        println!("Nothing to do");
+        return Ok(());
+    }
+
+    let cards = get_card_info(cache)?;
 
     step("Converting");
     let cards = cards
