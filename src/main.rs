bd9e92c4277d0da489ce85c05c255d41e49390c8
Reisz <Reisz@users.noreply.github.com>
2023-04-10 21:59:49 +0200

    add simple search

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use bincode::Options;
 use data::card::{Card, CardData};
 use gloo_net::http::Request;
 use leptos::{
-    component, create_local_resource, mount_to_body, view, For, ForProps, IntoView, Scope,
-    Suspense, SuspenseProps,
+    component, create_local_resource, create_node_ref, create_signal, html::Input, mount_to_body,
+    view, For, ForProps, IntoView, Scope, Suspense, SuspenseProps,
 };
 use lzma_rs::xz_decompress;
 
@@ -33,12 +33,37 @@ fn Card(cx: Scope, card: &'static Card) -> impl IntoView {
 }
 
 #[component]
-fn CardList(cx: Scope, cards: &'static CardData) -> impl IntoView {
+fn CardSearch(cx: Scope, cards: &'static CardData) -> impl IntoView {
+    let (filter, set_filter) = create_signal(cx, String::new());
+
+    let card_iter = move || {
+        let filter = filter();
+        cards
+            .iter()
+            .filter(move |(_, card)| {
+                if filter.is_empty() {
+                    return true;
+                }
+
+                card.name.contains(&filter)
+            })
+            .take(50) // TODO: implement paging
+    };
+
+    let input_ref = create_node_ref::<Input>(cx);
     view! {
         cx,
+        <input
+            type = "text"
+            node_ref = input_ref
+            on:input = move |_| {
+                let input = input_ref.get().unwrap();
+                set_filter(input.value());
+            }
+        />
         <div class="card-list">
             <For
-                each = move || cards.iter().take(50) // TODO: Limit for debug performance
+                each = card_iter
                 key = |(card_id, _)| *card_id
                 view = move |cx, (_, card)| view! {cx, <Card card = card />}
             />
@@ -54,7 +79,7 @@ fn App(cx: Scope) -> impl IntoView {
         cx,
         <Suspense fallback = move || "Loading...">
             {move || {
-                cards.read(cx).map(|cards| view!{cx, <CardList cards = cards />})
+                cards.read(cx).map(|cards| view!{cx, <CardSearch cards = cards />})
             }}
         </Suspense>
     }
