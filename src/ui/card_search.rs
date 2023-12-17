dcac2c88a24406c6a13a0c85d22cac64d360fafc
Reisz <Reisz@users.noreply.github.com>
2023-12-17 23:08:13 +0100

    Add pagination

--- src/ui/card_search.rs
+++ src/ui/card_search.rs
@@ -1,89 +1,140 @@
 use common::card::{Card, CardData};
 use leptos::{
-    component, create_node_ref, create_signal, expect_context, html, view, For, IntoView,
-    SignalUpdate, SignalWith,
+    component, create_memo, create_node_ref, create_signal, expect_context, html, provide_context,
+    view, For, IntoView, NodeRef, RwSignal, SignalGetUntracked, SignalSet, SignalWith,
+    SignalWithUntracked, WriteSignal,
 };
+use leptos_use::use_intersection_observer;
 
 use crate::ui::card_view::CardView;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, Copy)]
 struct CardFilter {
-    name: String,
-    text: String,
+    name: RwSignal<String>,
+    text: RwSignal<String>,
 }
 
 impl CardFilter {
     fn matches(&self, card: &Card) -> bool {
-        if !self.name.is_empty() && !card.name.to_ascii_lowercase().contains(&self.name) {
+        if self
+            .name
+            .with(|name| !name.is_empty() && !card.name.to_ascii_lowercase().contains(name))
+        {
             return false;
         }
 
-        if !self.text.is_empty() && !card.description.to_ascii_lowercase().contains(&self.text) {
+        if self
+            .text
+            .with(|text| !text.is_empty() && !card.description.to_ascii_lowercase().contains(text))
+        {
             return false;
         }
 
         true
     }
+}
 
-    fn set_name_filter(&mut self, filter: &str) {
-        self.name = filter.to_ascii_lowercase();
+#[derive(Clone, Copy)]
+struct ScrollReset {
+    set_pages: WriteSignal<usize>,
+    scroll_area: NodeRef<html::Div>,
+}
+
+impl ScrollReset {
+    fn reset(&self) {
+        let area = self.scroll_area.get().unwrap();
+        area.set_scroll_top(0);
+        self.set_pages.set(1);
     }
+}
 
-    fn set_text_filter(&mut self, filter: &str) {
-        self.text = filter.to_ascii_lowercase();
+#[component]
+#[must_use]
+pub fn FilterInput(
+    placeholder: &'static str,
+    map: fn(String) -> String,
+    filter: RwSignal<String>,
+) -> impl IntoView {
+    let node_ref = create_node_ref();
+    let reset = expect_context::<ScrollReset>();
+
+    view! {
+        <input
+            type="text"
+            placeholder=placeholder
+            ref=node_ref
+            on:input=move |_| {
+                let input = node_ref.get().unwrap();
+                reset.reset();
+                filter.set(map(input.value()));
+            }
+        />
     }
 }
 
 #[component]
 #[must_use]
 pub fn CardSearch() -> impl IntoView {
+    const PAGE_SIZE: usize = 50;
+
     let cards = expect_context::<&'static CardData>();
-
-    let (filter, set_filter) = create_signal(CardFilter::default());
-
-    let card_iter = move || {
+    let filter = CardFilter::default();
+    let filtered_cards = create_memo(move |_| {
         cards
             .iter()
-            .filter(move |(_, card)| filter.with(|filter| filter.matches(card)))
-            .take(50) // TODO: implement paging
+            .filter(move |(_, card)| filter.matches(card))
+            .map(|(id, _)| *id)
+            .collect::<Vec<_>>()
+    });
+
+    let (pages, set_pages) = create_signal(1);
+    let paginated_cards = move || {
+        filtered_cards.with(|cards| {
+            cards
+                .iter()
+                .copied()
+                .take(pages() * PAGE_SIZE)
+                .collect::<Vec<_>>()
+        })
     };
 
-    let name_input_ref = create_node_ref::<html::Input>();
-    let text_input_ref = create_node_ref::<html::Input>();
+    // Automatic pagination
+    let scroll_end_ref = create_node_ref::<html::Div>();
+    use_intersection_observer(scroll_end_ref, move |entries, _| {
+        let filtered_count = filtered_cards.with_untracked(Vec::len);
+        let current_capacity = pages.get_untracked() * PAGE_SIZE;
+        if entries[0].is_intersecting() && filtered_count > current_capacity {
+            set_pages(pages.get_untracked() + 1);
+        }
+    });
+
+    let scroll_area_ref = create_node_ref();
+    provide_context(ScrollReset {
+        set_pages,
+        scroll_area: scroll_area_ref,
+    });
+
     view! {
         <div class="card-search">
             <div class="card-search-params">
-                <input
-                    type="text"
-                    placeholder="Name"
-                    node_ref=name_input_ref
-                    on:input=move |_| {
-                        let input = name_input_ref.get().unwrap();
-                        set_filter.update(|filter| filter.set_name_filter(&input.value()));
-                    }
-                />
-
-                <input
-                    type="text"
+                <FilterInput placeholder="Name" map=|s| s.to_ascii_lowercase() filter=filter.name/>
+                <FilterInput
                     placeholder="Description"
-                    node_ref=text_input_ref
-                    on:input=move |_| {
-                        let input = text_input_ref.get().unwrap();
-                        set_filter.update(|filter| filter.set_text_filter(&input.value()));
-                    }
+                    map=|s| s.to_ascii_lowercase()
+                    filter=filter.text
                 />
-
             </div>
 
-            <div class="card-list">
+            <div class="card-list" ref=scroll_area_ref>
                 <For
-                    each=card_iter
-                    key=|(id, _)| *id
-                    children=move |(id, _)| {
-                        view! { <CardView id=*id/> }
+                    each=paginated_cards
+                    key=|id| *id
+                    children=move |id| {
+                        view! { <CardView id=id/> }
                     }
                 />
 
+                <div style="position:relative; top: -20rem;" ref=scroll_end_ref></div>
             </div>
         </div>
     }
