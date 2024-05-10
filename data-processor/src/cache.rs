ca2b238192284362505ebfe7f3281542793afb32
Reisz <Reisz@users.noreply.github.com>
2024-05-10 19:02:50 +0200

    Improve card info cache code

--- data-processor/src/cache.rs
+++ data-processor/src/cache.rs
@@ -7,34 +7,55 @@ use anyhow::Result;
 use serde::Deserialize;
 use tokio::{
     fs::{self, File},
-    io::{AsyncBufRead, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
     try_join,
 };
 
 use crate::{reqwest_indicatif::ProgressReader, step, ygoprodeck, CARD_INFO_LOCAL, OUTPUT_FILE};
 
-const VERSION_CHECK_INTERVAL: Duration = Duration::from_secs(60 * 60 * 24); // 1 day
-
+#[derive(Debug, Clone, Copy)]
 pub enum CacheResult {
     StillValid,
     ProcessingRequired,
 }
 
+async fn get_modification_time(path: impl AsRef<Path>) -> Result<SystemTime> {
+    Ok(fs::metadata(path).await?.modified()?)
+}
+
+/// Ensure that the cached card info is up to date.
+///
+/// This function also determines based on the state of the card info cache file and output file whether processing is required.
 pub async fn update_card_info_cache() -> Result<CacheResult> {
-    if let Some(version) = should_download().await? {
+    // Download a new version if required. Request processing in that case.
+    if let Some(version) = should_download_card_info().await? {
         step("Downloading cards");
-        let (mut response, mut file) = try_join!(
-            create_database_download(),
-            create_database_cache_writer(&version)
-        )?;
+
+        let database_download = async {
+            let response = reqwest::get(ygoprodeck::URL).await?.error_for_status()?;
+            Ok::<_, anyhow::Error>(BufReader::new(ProgressReader::from_response(response)))
+        };
+
+        let database_cache_writer = async {
+            let mut file = BufWriter::new(File::create(CARD_INFO_LOCAL).await?);
+            file.write_all(version.as_bytes()).await?;
+            file.write_all("\n".as_bytes()).await?;
+            Ok::<_, anyhow::Error>(file)
+        };
+
+        let (mut response, mut file) = try_join!(database_download, database_cache_writer)?;
         tokio::io::copy(&mut response, &mut file).await?;
+
         return Ok(CacheResult::ProcessingRequired);
     }
 
+    // If the output file is missing, request processing.
     if !Path::new(OUTPUT_FILE).try_exists()? {
         return Ok(CacheResult::ProcessingRequired);
     }
 
+    // If this executable is newer than the output file, request processing.
+    // This enables code changes to this executable to apply immediately.
     let (output_date, executable_date) = try_join!(
         get_modification_time(OUTPUT_FILE),
         get_modification_time(std::env::current_exe()?)
@@ -43,41 +64,33 @@ pub async fn update_card_info_cache() -> Result<CacheResult> {
         return Ok(CacheResult::ProcessingRequired);
     }
 
+    // Otherwise the output file should be up-to-date.
     Ok(CacheResult::StillValid)
 }
 
-async fn create_database_download() -> Result<impl AsyncBufRead> {
-    let response = reqwest::get(ygoprodeck::URL).await?.error_for_status()?;
-    Ok(BufReader::new(ProgressReader::from_response(response)))
-}
+/// Determine whether a new version of the card info cache needs to be downloaded.
+///
+/// This happens either because the cache is missing or has a different version. If neither is the case, this function will return `None`, otherwise it returns the version of the online data, so it can be stored with the new data.
+async fn should_download_card_info() -> Result<Option<String>> {
+    const VERSION_CHECK_INTERVAL: Duration = Duration::from_secs(60 * 60 * 24); // 1 day
 
-async fn create_database_cache_writer(version: &str) -> Result<impl AsyncWrite> {
-    let mut file = BufWriter::new(File::create(CARD_INFO_LOCAL).await?);
-    file.write_all(version.as_bytes()).await?;
-    file.write_all("\n".as_bytes()).await?;
-    Ok(file)
-}
-
-async fn get_modification_time(path: impl AsRef<Path>) -> Result<SystemTime> {
-    Ok(fs::metadata(path).await?.modified()?)
-}
-
-pub async fn should_download() -> Result<Option<String>> {
+    // If there is no cache, just get the online version and return.
     if !Path::new(CARD_INFO_LOCAL).try_exists()? {
         return Ok(Some(get_online_version().await?));
     }
 
-    let cache_date = fs::metadata(CARD_INFO_LOCAL).await?.modified()?;
-    if cache_date.elapsed()? > VERSION_CHECK_INTERVAL {
-        // Reset interval
-        std::fs::File::open(CARD_INFO_LOCAL)?.set_modified(SystemTime::now())?;
-
+    // The cache file exists. Check duration since the last update (via modification date).
+    if get_modification_time(CARD_INFO_LOCAL).await?.elapsed()? > VERSION_CHECK_INTERVAL {
         let (online_version, local_version) = try_join!(get_online_version(), get_local_version())?;
         if local_version != online_version {
             return Ok(Some(online_version));
         }
+
+        // The check succeded. Update modification time so we do not keep bugging the database.
+        std::fs::File::open(CARD_INFO_LOCAL)?.set_modified(SystemTime::now())?;
     }
 
+    // The cache does not need to be updated.
     Ok(None)
 }
 
--- data-processor/src/lib.rs
+++ data-processor/src/lib.rs
@@ -7,8 +7,11 @@ pub mod refine;
 pub mod reqwest_indicatif;
 pub mod ygoprodeck;
 
-pub const CARD_INFO_LOCAL: &str = "target/card_info.json";
+/// Location of the data used by the app.
 pub const OUTPUT_FILE: &str = "dist/cards.bin.xz";
+
+// Location of the cached card data download.
+pub const CARD_INFO_LOCAL: &str = "target/card_info.json";
 pub const MISSING_IMAGES: &str = "target/missing_images.bin";
 
 pub fn step(text: &str) {
